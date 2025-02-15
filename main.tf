906fb0ff0e39948aaaea09262fa1dee2174fb94b
root <root@ip-172-31-83-22.ec2.internal>
2025-02-15 10:51:04 +0000

    jenkina-terraform code

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1"  
+}
+
+resource "aws_instance" "foo" {
+  ami           = "ami-053a45fff0a704a47"
+  instance_type = "t2.micro"
+  tags = {
+      Name = "TF-Instance"
+  }
+}
