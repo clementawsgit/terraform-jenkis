10e1794a4b8c9514c2f67f07a200833d4d8875d5
clement <jeromeit05@gmail.com>
2025-02-20 12:13:34 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,12 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-#Creation of aws provider
-  provider "aws" {
+provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAWCZC5ZCVWFTGYAOM"
-  secret_key = "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"
 }
 
-#Creation of aws instance
-resource "aws_instance" "myec2" {
-instance_type = "t2.micro"
-key_name = "ubuntu"
-ami = "ami-04b4f1a9cf54c11d0"
-tags = {
-    Name = "clement-terraform"
-}
+resource "aws_instance" "example" {
+  ami           = "ami-04b4f1a9cf54c11d0"
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "Jenkins-EC2"
+  }
 }
